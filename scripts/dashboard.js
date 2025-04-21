01bfe488f8149d5462c6aca5244861aedeea2dbc
Zein Kassem <zein.s.kassem@gmail.com>
2025-04-21 21:51:45 +0300

    fetch+display users

---
+++ scripts/dashboard.js
@@ -0,0 +1,50 @@
+const users = JSON.parse(localStorage.getItem("users")) || [];
+const dashboardContainer = document.getElementById("dashboard-container");
+
+const user = JSON.parse(sessionStorage.getItem("loggedInUser"));
+const userName = document.getElementById("user-name");
+userName.innerHTML = `Admin`;
+
+// filter out admin
+const filteredUsers = users.filter((user) => user.email !== "admin@quiz.com");
+// console.log("Filtered Users (no admin):", filteredUsers);
+
+if (!filteredUsers.length) {
+  dashboardContainer.innerHTML = `<p class="no-exams">No User Registered.</p>`;
+} else {
+  let cardsHTML = `<div class="user-cards">`;
+
+  filteredUsers.forEach((user) => {
+    const fullName = `${user.name} ${user.lastName}`;
+    const email = user.email;
+
+    const quizScoresHTML =
+      user.scores && user.scores.length > 0
+        ? user.scores
+            .map(
+              (score) => `
+            <div class="quiz-item">
+              <strong>${score.title}</strong>
+              <span>${score.score} / ${score.numberOfQuestions}</span>
+            </div>
+          `
+            )
+            .join("")
+        : `<p class="no-scores">No Quizz taken yet</p>`;
+
+    cardsHTML += `
+      <div class="user-card">
+        <div class="user-info">
+          <h3>${fullName}</h3>
+          <p>${email}</p>
+        </div>
+        <div class="quiz-scores">
+          ${quizScoresHTML}
+        </div>
+      </div>
+    `;
+  });
+
+  cardsHTML += `</div>`;
+  dashboardContainer.innerHTML = cardsHTML;
+}
